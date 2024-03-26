c7ebf0a4c1fceb9c8a0ab1d845d35b91a99e282f
ekaterinatoporova04@mail.ru <ekaterinatoporova04@mail.ru>
2024-03-26 11:10:29 +0700

    Добавьте файлы проекта.

---
+++ вычмат_лаба3/Form1.Designer.cs
@@ -0,0 +1,218 @@
+﻿namespace вычмат_лаба3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.checkedListBox1 = new System.Windows.Forms.CheckedListBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(189, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(127, 20);
+            this.textBox1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(189, 38);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(127, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Ок";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(42, 285);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(127, 23);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Построить";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(10, 108);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(33, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "f(x) = ";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(19, 85);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(24, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "x = ";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.ColumnHeadersVisible = false;
+            this.dataGridView1.Location = new System.Drawing.Point(42, 81);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersVisible = false;
+            this.dataGridView1.ScrollBars = System.Windows.Forms.ScrollBars.Horizontal;
+            this.dataGridView1.Size = new System.Drawing.Size(530, 46);
+            this.dataGridView1.TabIndex = 6;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // checkedListBox1
+            // 
+            this.checkedListBox1.FormattingEnabled = true;
+            this.checkedListBox1.Items.AddRange(new object[] {
+            "Интерполяционный многочлен с помощью формулы Лагранжа",
+            "Интерполяционный многочлен с помощью формулы Ньютона",
+            "Сглаживающий многочлен 1 степени",
+            "Сглаживающий многочлен 2 степени",
+            "Сглаживающий многочлен 3 степени",
+            "Сглаживающий многочлен 1 степени, расчитанный вручную",
+            "Сглаживающий многочлен 2 степени, расчитанный вручную",
+            "Сглаживающий многочлен 3 степени, расчитанный вручную",
+            "Произвольный многочлен 4й степени"});
+            this.checkedListBox1.Location = new System.Drawing.Point(42, 185);
+            this.checkedListBox1.Name = "checkedListBox1";
+            this.checkedListBox1.Size = new System.Drawing.Size(370, 94);
+            this.checkedListBox1.TabIndex = 7;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(445, 285);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(127, 23);
+            this.button3.TabIndex = 8;
+            this.button3.Text = "Назад";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(39, 15);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(144, 13);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Введите количество точек:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(43, 149);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(145, 13);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "Введите интервал для x: от";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(189, 146);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(36, 20);
+            this.textBox2.TabIndex = 10;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(231, 150);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(19, 13);
+            this.label5.TabIndex = 13;
+            this.label5.Text = "до";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(252, 146);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(36, 20);
+            this.textBox3.TabIndex = 12;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(594, 322);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.checkedListBox1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.CheckedListBox checkedListBox1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBox3;
+    }
+}
+
---
+++ вычмат_лаба3/Form1.cs
@@ -0,0 +1,370 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace вычмат_лаба3
+{
+    public partial class Form1 : Form
+    {
+        List<float> x = new List<float>();
+        List<float> fx = new List<float>();
+        //Создаем элемент Chart
+        Chart myChart = new Chart();
+        
+
+
+        public Form1()
+        {
+            InitializeComponent();
+            //Добавляем chart на форму и растягиваем по ширине формы
+            myChart.Parent = this;
+            myChart.Dock = DockStyle.Fill;
+            //Добавляем в Chart область для рисования графиков
+            myChart.ChartAreas.Add(new ChartArea("Math functions"));
+            myChart.Hide();
+            button3.Hide();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+
+
+        //Метод Гаусса для решения матриц с выбором главного элемента по столбцу
+        public float[] Gauss(float[][] array)
+        {
+            int k = 0, index = 0;
+            float max = 0;
+            float[] a = new float[array.Count()];
+            for (int j = 0; j < array[0].Count(); j++)
+            {
+                //Ищем максимальный по модулю коэффициент
+                for (int i = 0; i < array.Count(); i++)
+                {
+                    if (Math.Abs(array[i][j]) > max)
+                    {
+                        max = array[i][j];
+                        a = array[i];
+                        index = i;
+                    }
+                }
+                //Меняем уравнения местами
+                array[index] = array[k];
+                array[k] = a;
+                max = 0;
+                float[][] array2 = new float[array.Count()][];
+                for (int i = 0; i < array.Count(); i++)
+                {
+                    array2[i] = new float[array[0].Count()];
+                    for (int h = 0; h < array[0].Count(); h++)
+                        array2[i][h] = array[i][h];
+                }
+                //Приводим систему к треугольной матрице
+                for (int i = k + 1; i < array.Count(); i++)
+                {
+                    for (int g = k; g < array[0].Count(); g++)
+                    {
+                        array[i][g] += array2[k][g] * (-array2[i][k] / array2[k][k]);
+                    }
+                }
+                k++;
+                if (k == array[0].Count() - 2) break;
+            }
+
+            //Обратный ход
+            float[] result = new float[array.Count()];
+            float summ = 0;
+            //Находим xi в каждом уравнении
+            for (int i = array.Count() - 1; i >= 0; i--)
+            {
+                for (int j = array.Count() - 1; j >= 0; j--)
+                    summ += array[i][j] * result[j];
+                result[i] = (array[i][array[0].Count() - 1] - summ) / array[i][i];
+                summ = 0;
+            }
+            return result;
+        }
+
+        //Поиск сглаживающего многочлена n-й степени
+        public float Smoothing_Polynomial(float xi, List <float> x, List<float> fx, int step)
+        {
+            int n = x.Count();
+            float[] c = new float[2 * step + 1]; float[] d = new float[step + 1];
+            float[][] a = new float[step + 1][];
+            float[] ai;
+            for (int m = 0; m <= 2 * step; m++)
+            {
+                for (int k = 0; k < n; k++)
+                {
+                    //Поиск с
+                    c[m] += (float)Math.Pow(x[k], m); 
+                    if (m <= step)
+                    {
+                        //Поиск d
+                        d[m] += fx[k] * (float)Math.Pow(x[k], m); 
+                    }
+                }
+            }
+
+            for (int i = 0; i <= step; i++)
+            {
+                a[i] = new float[step + 2];
+                for (int j = 0; j <= step; j++)
+                {
+                    //Заполняем матрицу  a значениями с
+                    a[i][j] = c[i + j]; 
+                }
+                //Свободные коээфициенты матрицы а - значения d
+                a[i][step + 1] = d[i];
+            }
+
+            ai = Gauss(a); //находим коэффициенты многочлена
+
+            float sum = ai[0];
+            //Подставляем значения x и ищем значения многочлена
+            for (int i = 1; i <= step; i++) 
+            {
+                float l = 1;
+                for (int j = 1; j <= i; j++)
+                {
+                    l *= xi;
+                }
+                sum += ai[i] * l;
+            }
+            return sum;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            dataGridView1.ColumnCount = Convert.ToInt32(textBox1.Text);
+            dataGridView1.RowCount = 2;
+            dataGridView1.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            float start = (float)Convert.ToDouble(textBox2.Text);
+            float end = (float)Convert.ToDouble(textBox3.Text);
+
+            for (int j = 0; j < dataGridView1.ColumnCount; j++)
+            {
+                x.Add((float)Convert.ToDouble(dataGridView1[j, 0].Value));
+                fx.Add((float)Convert.ToDouble(dataGridView1[j, 1].Value));
+            }
+           
+
+            float sum = 0;
+            foreach (int indexChecked in checkedListBox1.CheckedIndices)
+            {
+                //Построение многочлена с помощью Лагранжа
+                if (indexChecked == 0)
+                {
+                    //Создаем набор точек
+                    Series mySeriesOfPoint1 = new Series();
+                    mySeriesOfPoint1.ChartType = SeriesChartType.Line;
+                    mySeriesOfPoint1.ChartArea = "Math functions";
+                    mySeriesOfPoint1.BorderWidth = 3;
+                    //Подставляем значения x в многочлен - вычисляем точки
+                    for (float xi = start; xi <= end; xi += 0.01F)
+                    {                        
+                        sum = 0;
+                        for (int i = 0; i < x.Count(); i++)
+                        {
+                            float l = 1;
+                            for (int j = 0; j < x.Count(); j++)
+                            {
+                                if (j != i)
+                                    l *= (xi - x[j]) / (x[i] - x[j]);
+                            }
+                            sum += l * fx[i];
+                        }
+                        mySeriesOfPoint1.Points.AddXY(xi, sum);
+                    }
+                    //Добавляем созданный набор точек в Chart
+                    myChart.Series.Add(mySeriesOfPoint1);
+                }
+
+                //Построение многочлена с помощью Ньютона
+                if (indexChecked == 1)
+                {
+                    Series mySeriesOfPoint2 = new Series();
+                    mySeriesOfPoint2.ChartType = SeriesChartType.Line;
+                    mySeriesOfPoint2.ChartArea = "Math functions";
+                    mySeriesOfPoint2.BorderWidth = 4;
+                    for (float xi = start; xi <= end; xi += 0.01F)
+                    {
+                        sum = fx[0];
+                        for (int i = 1; i < x.Count(); ++i) //подставляем значения х в многочлен
+                        {
+                            float F = 0;
+                            for (int j = 0; j <= i; ++j)
+                            {
+
+                                float den = 1;
+                                for (int k = 0; k <= i; ++k)
+                                    if (k != j)
+                                        den *= (x[j] - x[k]);
+                                F += fx[j] / den;
+                            }
+
+                            for (int k = 0; k < i; ++k)
+                                F *= (xi - x[k]);
+                            sum += F;
+                        }
+                        mySeriesOfPoint2.Points.AddXY(xi, sum);
+                    }
+                    myChart.Series.Add(mySeriesOfPoint2);
+                }
+
+                //Сглаживающий многочлен 1 степени
+                if (indexChecked == 2)
+                {
+                    Series mySeriesOfPoint3 = new Series();
+                    mySeriesOfPoint3.ChartType = SeriesChartType.Line;
+                    mySeriesOfPoint3.ChartArea = "Math functions";
+                    mySeriesOfPoint3.BorderWidth = 5;
+                    for (float xi = start; xi <= end; xi += 0.01F)
+                    {
+                        mySeriesOfPoint3.Points.AddXY(xi, Smoothing_Polynomial(xi, x, fx, 1));
+                    }
+                    myChart.Series.Add(mySeriesOfPoint3);
+                }
+
+                //Сглаживающий многочлен 2 степени
+                if (indexChecked == 3)
+                {
+                    Series mySeriesOfPoint4 = new Series();
+                    mySeriesOfPoint4.ChartType = SeriesChartType.Line;
+                    mySeriesOfPoint4.ChartArea = "Math functions";
+                    mySeriesOfPoint4.BorderWidth = 6;
+                    for (float xi = start; xi <= end; xi += 0.01F)
+                    {
+                        mySeriesOfPoint4.Points.AddXY(xi, Smoothing_Polynomial(xi, x, fx, 2));
+                    }
+                    myChart.Series.Add(mySeriesOfPoint4);
+                }
+
+                //Сглаживающий многочлен 3 степени
+                if (indexChecked == 4)
+                {
+                    Series mySeriesOfPoint5 = new Series();
+                    mySeriesOfPoint5.ChartType = SeriesChartType.Line;
+                    mySeriesOfPoint5.ChartArea = "Math functions";
+                    mySeriesOfPoint5.BorderWidth = 7;
+                    for (float xi = start; xi <= end; xi += 0.01F)
+                    {
+                        mySeriesOfPoint5.Points.AddXY(xi, Smoothing_Polynomial(xi, x, fx, 3));
+                    }
+                    myChart.Series.Add(mySeriesOfPoint5);
+                }
+
+                //Сглаживающий многочлен 1 степени, расчитанный вручную
+                if (indexChecked == 5)
+                {
+                    Series mySeriesOfPoint6 = new Series();
+                    mySeriesOfPoint6.ChartType = SeriesChartType.Line;
+                    mySeriesOfPoint6.ChartArea = "Math functions";
+                    mySeriesOfPoint6.BorderWidth = 7;
+                    for (float xi = start; xi <= end; xi += 0.01F)
+                    {
+                        float yi = -3.19f * xi + 11.466f;
+                        mySeriesOfPoint6.Points.AddXY(xi, yi);
+                    }
+                    myChart.Series.Add(mySeriesOfPoint6);
+                }
+
+                //Сглаживающий многочлен 2 степени, расчитанный вручную
+                if (indexChecked == 6)
+                {
+                    Series mySeriesOfPoint7 = new Series();
+                    mySeriesOfPoint7.ChartType = SeriesChartType.Line;
+                    mySeriesOfPoint7.ChartArea = "Math functions";
+                    mySeriesOfPoint7.BorderWidth = 7;
+                    for (float xi = start; xi <= end; xi += 0.01F)
+                    {
+                        float yi = 0.045f * (float)Math.Pow(xi, 2) - 3.276f * xi + 11.291f;
+                        mySeriesOfPoint7.Points.AddXY(xi, yi);
+                    }
+                    myChart.Series.Add(mySeriesOfPoint7);
+                }
+
+                //Сглаживающий многочлен 3 степени, расчитанный вручную
+                if (indexChecked == 7)
+                {
+                    Series mySeriesOfPoint8 = new Series();
+                    mySeriesOfPoint8.ChartType = SeriesChartType.Line;
+                    mySeriesOfPoint8.ChartArea = "Math functions";
+                    mySeriesOfPoint8.BorderWidth = 7;
+                    for (float xi = start; xi <= end; xi += 0.01F)
+                    {
+                        float yi = 0.059f * (float)Math.Pow(xi, 3) - 0.138f * (float)Math.Pow(xi, 2) - 3.539f * xi + 11.789f;
+                        mySeriesOfPoint8.Points.AddXY(xi, yi);
+                    }
+                    myChart.Series.Add(mySeriesOfPoint8);
+                }
+
+                //Произвольный многочлен 4й степени
+                if (indexChecked == 8)
+                {
+                    Series mySeriesOfPoint9 = new Series();
+                    mySeriesOfPoint9.ChartType = SeriesChartType.Line;
+                    mySeriesOfPoint9.ChartArea = "Math functions";
+                    mySeriesOfPoint9.BorderWidth = 7;
+                    for (float xi = start; xi <= end; xi += 0.01F)
+                    {
+                        float yi = 0.329f * (float)Math.Pow(xi, 4) - 1.379f * (float)Math.Pow(xi, 3) - 1.192f * (float)Math.Pow(xi, 2) + 2.767f * xi + 12;
+                        mySeriesOfPoint9.Points.AddXY(xi, yi);
+                    }
+                    myChart.Series.Add(mySeriesOfPoint9);
+                }
+            }
+
+            Series serie = new Series();
+            //Добавляем точки из таблицы
+            for (int i = 0; i < x.Count(); i++)
+            {
+                serie.ChartType = SeriesChartType.Point;
+                // DataPoint dp = new DataPoint(x[i], fx[i]);
+                serie.MarkerStyle = MarkerStyle.Circle;
+                serie.MarkerSize = 6;
+                serie.MarkerColor = Color.Black;
+                serie.IsValueShownAsLabel = true; //Показать значение в точке
+                serie.Points.AddXY(x[i], fx[i]);               
+            }
+            myChart.Series.Add(serie);
+            this.Height = this.Width;
+            myChart.Show();
+            button3.Show();
+            myChart.BringToFront();
+            button3.BringToFront();
+            
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            this.myChart.Series.Clear();
+            foreach (var series in myChart.Series)
+            {
+                series.Points.Clear();                
+            }
+            myChart.Hide();
+            button3.Hide();
+            this.Height = 361;
+        }
+    }
+}
